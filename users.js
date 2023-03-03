23b45568915c5abf5f5b98348ebf1f8c5f2ed48a
Ishini De Silva <101697017+Ishini0818@users.noreply.github.com>
2023-03-03 10:03:47 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,29 @@
+function showPass() {
+  var x = document.getElementById("pword");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function users() {
+  var usernames = ["Ishini"];
+  var passwords = ["1234"];
+  var x = document.getElementById("username").value;
+  var y = document.getElementById("pword").value;
+
+  for (var i = 0; i < usernames.length; i++) {
+    for (var j = 0; j < usernames.length; j++) {
+      if (x == usernames[i] && y == passwords[j]) {
+        location.replace("home.html");
+        break;
+      } else {
+        alert("Invalid User Name or Password!");
+        break;
+      }
+    }
+  }
+}
+
+
